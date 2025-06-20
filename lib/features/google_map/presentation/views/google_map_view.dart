a625c82c4e1931080181c5b65ef770b92bfb1943
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2025-06-20 17:59:06 +0300

    Implement location tracking and update current location on Google Map

--- lib/features/google_map/presentation/views/google_map_view.dart
+++ lib/features/google_map/presentation/views/google_map_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:route_tracker_app/core/utils/location_service.dart';
 
 class GoogleMapView extends StatefulWidget {
   const GoogleMapView({super.key});
@@ -10,18 +11,54 @@ class GoogleMapView extends StatefulWidget {
 
 class _GoogleMapViewState extends State<GoogleMapView> {
   late CameraPosition initalCameraPosition;
+  late LocationService locationService;
+  GoogleMapController? googleMapController;
+
+  Set<Marker> markers = {};
 
   @override
   void initState() {
     initalCameraPosition = CameraPosition(target: LatLng(0, 0));
+    locationService = LocationService();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      markers: markers,
       zoomControlsEnabled: false,
       initialCameraPosition: initalCameraPosition,
+      onMapCreated: (controller) {
+        googleMapController = controller;
+        updateCurrentLocation();
+      },
     );
+  }
+
+  void updateCurrentLocation() async {
+    try {
+      var locationData = await locationService.getLocation();
+
+      LatLng currentPosition = LatLng(locationData.latitude!, locationData.longitude!);
+      Marker currentLocationMarker = Marker(
+        markerId: MarkerId('my Location'),
+        position: currentPosition,
+        icon: BitmapDescriptor.defaultMarker,
+      );
+      CameraPosition myCurrentCameraPosition = CameraPosition(
+        target: LatLng(locationData.latitude!, locationData.longitude!),
+        zoom: 16,
+      );
+      googleMapController?.animateCamera(CameraUpdate.newCameraPosition(myCurrentCameraPosition));
+      markers.add(currentLocationMarker);
+      setState(() {});
+    } on LocationServiceException catch (e) {
+      // TODO:
+    } on LocationPermissionException catch (e) {
+      // TODO:
+    } catch (e) {
+      // TODO:
+    }
   }
 }
